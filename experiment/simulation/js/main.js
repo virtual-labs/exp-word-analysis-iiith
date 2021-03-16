5ddfc3f428e6396606eebbd103676010e5845632
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2021-03-16 21:24:56 +0530

    Updated simulation

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,45 @@
-//Your JavaScript goes in here
+var index;
+
+        function getSelectedValue() {
+            getdata(document.getElementById('userWord').value);
+            return false;
+        }
+
+        function getdata(ind) {
+            if (ind == 'null') {
+                alert("Select word");
+                document.getElementById("fldiv").innerHTML = "";
+                return;
+            }
+            $('#fldiv').load('exp1.php?index=' + ind + '&root=%&category=%&gender=%&form=%&person=%&tense=%&reference=%&turn=%');
+        }
+
+        var lang;
+        var src;
+
+        function getOption(temp) {
+            temp1 = temp.split("_");
+            lang = temp1[0];
+            scr = temp1[1];
+            document.getElementById("option").innerHTML = "";
+            document.getElementById("fldiv").innerHTML = "";
+
+            if (lang == "null") {
+                alert("Select language");
+                return;
+            }
+            $('#option').load('exp1_opt.php?lang=' + lang + '&script=' + scr);
+        }
+    
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-67558473-1', 'auto');
+        ga('send', 'pageview');
